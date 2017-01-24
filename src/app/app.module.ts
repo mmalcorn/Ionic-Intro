e32576a556c827bb0719cd63dab03a91d663f126
Meredith Alcorn <meredithalcorn@Merediths-MacBook-Pro.local>
2017-01-24 09:23:30 -0800

    change the About tab to be a Profile tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
+import { ProfilePage } from '../pages/profile/profile';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -9,7 +9,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    ProfilePage,
     ContactPage,
     HomePage,
     TabsPage
@@ -20,7 +20,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    ProfilePage,
     ContactPage,
     HomePage,
     TabsPage
